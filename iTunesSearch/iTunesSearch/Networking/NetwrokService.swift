d647743ccfa7eb2096bd14739f51a485b919dc1c
Matthew Hill <matthill265@gmail.com>
2023-03-03 15:21:10 -0700

    Creates NetworkService fetchAlbum function

--- iTunesSearch/iTunesSearch/Networking/NetwrokService.swift
+++ iTunesSearch/iTunesSearch/Networking/NetwrokService.swift
@@ -6,3 +6,44 @@
 //
 
 import UIKit
+
+class NetworkingService {
+    
+    static func fetchAlbums(forArtist search: String, completion: @escaping (Result<TopLevelResults, NetworkError>) -> Void) {
+        
+        //https://itunes.apple.com/search?entity=album&limit=25&term=Circa%20Survive
+        guard let baseURL = URL(string: Constants.ItunesURL.baseURL) else {completion(.failure(.invalidURL)); return}
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        urlComponents?.path.append(contentsOf: Constants.ItunesURL.searchPath)
+        
+        let albumQuery = URLQueryItem(name: Constants.QueryItems.queryAlbumKey, value: Constants.QueryItems.queryAlbumValue)
+        let limitQuery = URLQueryItem(name: Constants.QueryItems.queryLimitKey, value: Constants.QueryItems.queryLimitValue)
+        let artistQuery = URLQueryItem(name: Constants.QueryItems.queryArtistKey, value: search)
+        urlComponents?.queryItems = [albumQuery, limitQuery, artistQuery]
+        
+        guard let finalURL = urlComponents?.url else {completion(.failure(.invalidURL)); return}
+        print(finalURL)
+        
+        URLSession.shared.dataTask(with: finalURL) { data, response, error in
+            
+            if let error = error {
+                completion(.failure(.thrownError(error)))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print(response.statusCode)
+            }
+            
+            guard let data = data else {completion(.failure(.noData)); return}
+            
+            do {
+                let topLevel = try JSONDecoder().decode(TopLevelResults.self, from: data)
+                completion(.success(topLevel))
+            } catch {
+                completion(.failure(.unableToDecode))
+                return
+            }
+        }.resume()
+    }
+}
