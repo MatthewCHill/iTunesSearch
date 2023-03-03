92ea025cfea632bcbfc9d82b785ccba092c2cba4
Matthew Hill <matthill265@gmail.com>
2023-03-03 14:30:16 -0700

    Creates model for ArtistAlbum

---
+++ iTunesSearch/iTunesSearch/Model/AritistAlbum.swift
@@ -0,0 +1,29 @@
+//
+//  AritistAlbum.swift
+//  iTunesSearch
+//
+//  Created by Matthew Hill on 3/3/23.
+//
+
+import Foundation
+
+struct TopLevelResults: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case results
+    }
+    let results: [Albums]
+}
+
+struct Albums: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case albumID = "collectionId"
+        case albumName = "collectionName"
+        case trackCount
+        case albumArt = "artworkUrl100"
+    }
+    
+    let albumID: Int
+    let albumName: String
+    let trackCount: Int
+    let albumArt: String
+}
